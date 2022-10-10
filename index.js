4ce7146588101cedca9c48e53ddab714a79f58c8
Rahul <rahulkashyap4019@gmail.com>
2022-10-10 07:44:41 +0530

    Setup express router

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const port = 5000;
-
 const app = express();
+const port = 8000;
+
+// use express router
+app.use("/", require("./routes"));
 
 app.listen(port, (err) => {
   if (err) {
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+
+
+console.log('router reloaded')
+
+module.exports = router;
