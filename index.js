8929cf6a807fdf0c877d2e7f438a046cbe679fe5
Rahul <rahulkashyap4019@gmail.com>
2022-10-10 08:32:19 +0530

    Implement partials using layout in views

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressLayouts = require("express-ejs-layouts");
+
+app.use(expressLayouts);
 
 // set up view engine
-app.set('view engine', "ejs")
-app.set("views", "./views")
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // use express router
 app.use("/", require("./routes"));
